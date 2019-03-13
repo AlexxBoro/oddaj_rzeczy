92fd21c5a0fef4211ae8bcc4edf2491772542cd1
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-03-13 22:51:38 +0100

    Updates

---
+++ precache-manifest.ed1eb14b53eaa702565a05b6cfc10e0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c60c6fe9605a0b26170b834fdc6244f",
+    "url": "/oddaj_rzeczy/static/media/header_start_img.2c60c6fe.jpg"
+  },
+  {
+    "revision": "85c98296eeecd76a9bf40cb6c5827b20",
+    "url": "/oddaj_rzeczy/static/media/header_logged_img.85c98296.jpg"
+  },
+  {
+    "revision": "74c241482c275f28baa081947a4616db",
+    "url": "/oddaj_rzeczy/static/media/footer_bg_img.74c24148.jpg"
+  },
+  {
+    "revision": "0e48fc7f4f6fe85f5d1a34bdb30a84af",
+    "url": "/oddaj_rzeczy/static/media/about.0e48fc7f.jpg"
+  },
+  {
+    "revision": "4e6656e737c8b9d21f27",
+    "url": "/oddaj_rzeczy/static/js/runtime~main.371b45f9.js"
+  },
+  {
+    "revision": "6832d40abd8182bfb777",
+    "url": "/oddaj_rzeczy/static/js/main.221b324a.chunk.js"
+  },
+  {
+    "revision": "7f04c1311448c30ef17f",
+    "url": "/oddaj_rzeczy/static/js/2.fe037a24.chunk.js"
+  },
+  {
+    "revision": "6832d40abd8182bfb777",
+    "url": "/oddaj_rzeczy/static/css/main.e1fbd992.chunk.css"
+  },
+  {
+    "revision": "9277a47fb6e6075baee33ac40a805ecc",
+    "url": "/oddaj_rzeczy/index.html"
+  }
+];
