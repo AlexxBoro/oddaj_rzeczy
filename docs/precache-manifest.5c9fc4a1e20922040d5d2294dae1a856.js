f8226769b93585e937dad3bb107583277f5bad13
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-03-13 14:15:57 +0100

    created docs folder to deploy (build)

---
+++ docs/precache-manifest.5c9fc4a1e20922040d5d2294dae1a856.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c60c6fe9605a0b26170b834fdc6244f",
+    "url": "/static/media/header_start_img.2c60c6fe.jpg"
+  },
+  {
+    "revision": "74c241482c275f28baa081947a4616db",
+    "url": "/static/media/footer_bg_img.74c24148.jpg"
+  },
+  {
+    "revision": "0e48fc7f4f6fe85f5d1a34bdb30a84af",
+    "url": "/static/media/about.0e48fc7f.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "779e4874d741f422e193",
+    "url": "/static/js/main.5d9b4116.chunk.js"
+  },
+  {
+    "revision": "8017489636f06fd55ef1",
+    "url": "/static/js/2.ebe4a9df.chunk.js"
+  },
+  {
+    "revision": "779e4874d741f422e193",
+    "url": "/static/css/main.85d32ac4.chunk.css"
+  },
+  {
+    "revision": "6f983489587929446f8e916aed8f3080",
+    "url": "/index.html"
+  }
+];
