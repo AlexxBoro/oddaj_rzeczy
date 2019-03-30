1a92c5b1d2d74ab5b71586371620c12599a10e90
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-03-30 11:06:50 +0100

    amendments to about-us section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import './AboutUs.scss';
+import React from "react";
+import "./AboutUs.scss";
 
 class AboutUs extends React.Component {
-    render(){
-        return(
-            <div id="about-us">
+  render() {
+    return (
+      <div id="about-us" className="about-us">
+        <div className="about-us-container">
+          <div className="about-us-text">
+            <h1 className="about-us-text__title">O nas</h1>
 
-            <div className="section4_container">
-
-                <div className="text_container">
-                    <div className="section4_tytul">O nas</div>
-
-                    <div className="znaczek_container_section4">
-                        <div className="znaczek_section4"></div>
-                    </div>
-
-                    <div className="section4_text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel nibh imperdiet, mollis metus sit amet, ornare quam. Sed malesuada tincidunt sapien.</p>
-                    </div>
-                </div>
-
-
-                <div className="image_container"></div>
-
+            <div className="about-us__decoration-container">
+              <div className="about-us__decoration" />
             </div>
-            
-            </div>
-        )
-    }
+
+            <p className="about-us-text__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel
+              nibh imperdiet, mollis metus sit amet, ornare quam. Sed malesuada
+              tincidunt sapien.
+            </p>
+          </div>
+
+          <div className="about-us__image" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AboutUs;
